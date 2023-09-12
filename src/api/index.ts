e47007661fd16c08983e777f841d0cce413da275
Phil Cockfield <phil@cockfield.net>
2023-09-12 12:54:53 +1200

    clena up sample

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,22 @@
-import Pkg from '../../package.json' assert { type: 'json' };
-// import { DEFAULTS } from '../common.js';
-
+import { DEFAULTS } from '../common.js';
 export const runtime = 'edge';
 
+/**
+ * Sample
+ */
 export async function GET(request: Request) {
-  const name = Pkg.name;
-  const version = Pkg.version;
-  const payload = { name, version };
+  const payload = {
+    foo: DEFAULTS.foo,
+  };
   return HttpResponse.json(200, payload);
 }
 
+/**
+ * Helpers
+ */
 const HttpResponse = {
   json(status: number, data: any) {
     const json = JSON.stringify(data || {});
     return new Response(json, { status, headers: { 'Content-Type': 'application/json' } });
   },
-} as const;
+};
