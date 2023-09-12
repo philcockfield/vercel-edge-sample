bd06cc4382e3a412f9693959ccea7e34c0f1b8b5
Phil Cockfield <phil@cockfield.net>
2023-09-12 12:41:39 +1200

    breaking import (sample)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,12 @@
 import Pkg from '../../package.json' assert { type: 'json' };
+import { DEFAULTS } from '../common.js';
 
 export const runtime = 'edge';
 
 export async function GET(request: Request) {
   const name = Pkg.name;
   const version = Pkg.version;
-  return HttpResponse.json(200, { name, version });
+  return HttpResponse.json(200, { name, version, DEFAULTS });
 }
 
 const HttpResponse = {
